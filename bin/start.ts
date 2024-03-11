c888cbc98bcb8c8dac69496e4eb5fb2674ec2587
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2024-03-11 03:42:14 +0000

    CDK

---
+++ bin/start.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { AppStack } from '../lib/stack';
+
+const app = new cdk.App();
+new AppStack(app, 'AppStack', {
+    description: "My test app stack",
+    bucketName: "TestBucket",
+    tableName: "TestTable",
+    lambdaName: "TestLambda",
+    apiName: "TestApi"
+});
---
+++ lib/stack.ts
@@ -0,0 +1,40 @@
+import * as cdk from 'aws-cdk-lib';
+import {
+  aws_dynamodb as dynamodb,
+  aws_s3 as s3,
+  aws_lambda as lambda,
+  aws_apigateway as apigateway,
+} from 'aws-cdk-lib';
+import * as path from 'path';
+import { Construct } from 'constructs';
+
+export interface AppStackProps extends cdk.StackProps {
+  readonly bucketName: string;
+  readonly tableName: string;
+  readonly lambdaName: string;
+  readonly apiName: string;
+}
+
+export class AppStack extends cdk.Stack {
+
+  constructor(scope: Construct, id: string, props: AppStackProps) {
+    super(scope, id, props);
+
+    const bucket = new s3.Bucket(this, props.bucketName, {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const table = new dynamodb.Table(this, props.tableName, {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+    const lambdaFunction = new lambda.Function(this, props.lambdaName, {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
+      handler: 'functions.handler',
+    });
+    const api = new apigateway.RestApi(this, props.apiName);
+    const lambdaIntegration = new apigateway.LambdaIntegration(lambdaFunction);
+    api.root.addMethod('GET', lambdaIntegration);
+  }
+}
