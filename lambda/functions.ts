baf89fe39545f392bd6950be1fa46b013eb0a0a9
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2024-04-01 03:12:17 +0000

    DB works with hard coded table name

--- lambda/functions.ts
+++ lambda/functions.ts
@@ -1,78 +1,94 @@
-import { Handler } from 'aws-lambda';
-import { DynamoDB } from 'aws-sdk';
+import { Handler, APIGatewayProxyEvent } from 'aws-lambda';
+import { DynamoDBClient, PutItemCommand, PutItemCommandInput, QueryCommand, QueryCommandInput } from '@aws-sdk/client-dynamodb';
 
-const ddb = new DynamoDB()
-const tableName = 'MyTable'
+const ddb = new DynamoDBClient()
 
-export const SaveToDb: Handler = async (event, context) => {
-    const pk = event['queryStringParameters']['pk']
-    const sk = event['queryStringParameters']['sk']
-    const value = event['queryStringParameters']['value']
-    if (!pk || !sk || !value) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: "Invalid request." })
-        }
-    }
+// TODO: Change to environment with dynamic variable
+const tableName = 'arn:aws:dynamodb:us-west-1:919197864738:table/AppStack-MyTable794EDED1-1H0BX04UT5YX0'
+
+export const SaveToDb: Handler<APIGatewayProxyEvent> = async (event) => {
+    const params = event.queryStringParameters
 
     let result = {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Save to DDB!' }),
+        statusCode: 400,
+        body: JSON.stringify({ message: "Invalid request." })
     }
-    ddb.putItem({
-        TableName: tableName,
-        Item: { pk, sk, value }
-    }, (err) => {
-        if (err) {
+    console.log(params)
+
+    if (params && params['pk'] && params['sk'] && params['value']) {
+        const pk = params['pk']
+        const sk = params['sk']
+        const value = params['value']
+
+        result = {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Save to DDB!' }),
+        }
+        const input: PutItemCommandInput = {
+            TableName: tableName,
+            Item: {
+                'pk': { 'S': pk },
+                'sk': { 'S': sk },
+                'value': { 'S': value }
+            }
+        }
+        const command = new PutItemCommand(input)
+        try {
+            const response = await ddb.send(command)
+        } catch (err: any) {
             result = {
                 statusCode: 500,
                 body: JSON.stringify({ message: err.message })
             }
         }
-    })
-
+    }
     return result;
 };
 
-export const FetchFromDb: Handler = async (event, context) => {
-    const pk = event['queryStringParameters']['pk']
-    const sk = event['queryStringParameters']['sk']
-
-    if (!pk || !sk) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: "Invalid request." })
-        }
-    }
+export const FetchFromDb: Handler<APIGatewayProxyEvent> = async (event) => {
+    const params = event.queryStringParameters
 
     let result = {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Fetched with empty result.' }),
+        statusCode: 400,
+        body: JSON.stringify({ message: "Invalid request." })
     }
 
-    ddb.query({
-        TableName: tableName,
-        KeyConditionExpression: "pk = :a and sk = :t",
-        ExpressionAttributeValues: {
-            ":a": pk,
-            ":t": sk
+    if (params && params['pk'] && params['sk']) {
+        const pk = params['pk']
+        const sk = params['sk']
+
+        result = {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Fetched with empty result.' }),
         }
-    }, (err, data) => {
-        if (err) {
-            result = {
-                statusCode: 500,
-                body: JSON.stringify({ message: err.message })
+        const input: QueryCommandInput = {
+            TableName: tableName,
+            KeyConditionExpression: "pk = :a and sk = :t",
+            ExpressionAttributeValues: {
+                ":a": { 'S': pk },
+                ":t": { 'S': sk }
             }
-        } else {
-            if (data.Items && data.Items.length > 0 && data.Items[0]) {
-                const value = data.Items[0]['value']
+        }
+        const command = new QueryCommand(input)
+        try {
+            const response = await ddb.send(command)
+
+            if (response.Items && response.Items.length > 0 && response.Items[0]) {
+                const value = response.Items[0]['value']
                 result = {
                     statusCode: 200,
                     body: JSON.stringify({ message: `Fetched value: ${value}` }),
                 }
             }
+        } catch (err: any) {
+            if (err) {
+                result = {
+                    statusCode: 500,
+                    body: JSON.stringify({ message: err.message })
+                }
+            }
         }
-    })
+    }
 
     return result
 };
--- lib/stack.ts
+++ lib/stack.ts
@@ -4,15 +4,18 @@ import {
   aws_s3 as s3,
   aws_lambda as lambda,
   aws_apigateway as apigateway,
+  aws_iam as iam,
   StackProps,
 } from 'aws-cdk-lib';
 import * as path from 'path';
 import { Construct } from 'constructs';
+import { TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 
 const createLambdaWithApig = (
   stack: AppStack,
   baseName: string,
-  httpMethod: string
+  httpMethod: string,
+  table: TableV2
 ) => {
   const lambdaFunction = new lambda.Function(stack, 'Func' + baseName, {
     runtime: lambda.Runtime.NODEJS_20_X,
@@ -27,6 +30,7 @@ const createLambdaWithApig = (
   const lambdaIntegration = new apigateway.LambdaIntegration(lambdaFunction);
 
   api.root.addMethod(httpMethod, lambdaIntegration);
+  table.grantReadWriteData(lambdaFunction)
 }
 
 export class AppStack extends cdk.Stack {
@@ -36,16 +40,15 @@ export class AppStack extends cdk.Stack {
     const bucket = new s3.Bucket(this, "AppBucket", {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
-    console.log(bucket.bucketName);
 
     const table = new dynamodb.TableV2(this, "MyTable", {
       partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
       billing: dynamodb.Billing.onDemand(),
-      removalPolicy: cdk.RemovalPolicy.RETAIN
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
-    createLambdaWithApig(this, "SaveToDb", "GET");
-    createLambdaWithApig(this, "FetchFromDb", "GET");
+    createLambdaWithApig(this, "SaveToDb", "GET", table);
+    createLambdaWithApig(this, "FetchFromDb", "GET", table);
   }
 }
